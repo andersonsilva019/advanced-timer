14703b34a3a970925b1187d832e5527f532c568a
Anderson Silva <andersonnsilva@alu.ufc.br>
2022-07-21 21:33:33 -0300

    Cores & fontes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-export function App() {
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./styles/GlobalStyles";
+import { defaultTheme } from "./styles/themes/default";
 
+export function App() {
   return (
-    <h1>Hello</h1>
+    <>
+      <ThemeProvider theme={defaultTheme}>
+        <h1>Hello</h1>
+        <GlobalStyles />
+      </ThemeProvider>
+    </>
   )
 }
 

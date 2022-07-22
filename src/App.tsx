ea19b3cf8afb016a3798c5213f0b892578b5396d
Anderson Silva <andersonnsilva@alu.ufc.br>
2022-07-22 07:57:37 -0300

    setup React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "./styles/GlobalStyles";
 import { defaultTheme } from "./styles/themes/default";
+import { Router } from "./Routes";
 
 export function App() {
   return (
     <>
       <ThemeProvider theme={defaultTheme}>
-        <h1>Hello</h1>
+        <Router />
         <GlobalStyles />
       </ThemeProvider>
     </>
